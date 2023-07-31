038e6c2fca815ae69a6f5462e3318e56f96fdc55
daisiyao <daisiyao@hyperchain.cn>
2023-07-31 16:41:43 +0800

    feat: 修改enum

--- src/common/enum.js
+++ src/common/enum.js
@@ -40,31 +40,6 @@ export const DIALOG_FORM_REF_TYPE = {
 };
 
 export const FORM_TYPES = {
-  // SELECT: {
-  //     code: 'SELECT',
-  //     desc: '选择框'
-  // },
-  // INPUT: {
-  //     code: 'INPUT',
-  //     desc: '输入框'
-  // },
-  // RADIO: {
-  //     code: 'RADIO',
-  //     desc: '单选'
-  // },
-  // DATE: {
-  //     code: 'DATE',
-  //     desc: '时间选择器'
-  // },
-  // TIME: {
-  //     code: 'TIME',
-  //     desc: '日时分选择器'
-  // },
-  // FILE: {
-  //     code: 'FILE',
-  //     desc: '文件'
-  // },
-
   INPUT: {
     code: "INPUT",
     desc: "输入框",
